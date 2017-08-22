b6611143c5f5d33521a9c1441e1dcfb4e6f70d58
Kyle Atmore <atmorek@gmail.com>
2017-08-22 19:39:01 -0400

    Add reset style sheet and align inputs on login form

--- frontend/components/auth/sign_up_part1.jsx
+++ frontend/components/auth/sign_up_part1.jsx
@@ -6,24 +6,30 @@ const SignUpPart1 = ({ handleInput, handleSubmit, email, password}) => {
 
       <h2>Your Account Information</h2>
       <form className="signup-form">
-        <label>Email:
-          <input
-            type="text"
-            onChange={handleInput('email')}
-            value={email}
-            />
-        </label>
+        <ul className="input-fields">
+          <li className="field">
+            <label>Email Address:</label>
+            <input
+              type="text"
+              onChange={handleInput('email')}
+              value={email}
+              />
+          </li>
 
-        <label>Password:
-          <input
-            type="password"
-            onChange={handleInput('password')}
-            value={password}
-            />
-        </label>
+          <li className="field">
+            <label>Password:</label>
+            <input
+              type="password"
+              onChange={handleInput('password')}
+              value={password}
+              />
+          </li>
+        </ul>
 
         <input
+          className="button"
           type="submit"
+          value="Continue"
           onClick={handleSubmit}
         />
       </form>
