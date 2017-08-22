b38fc255897cefb9de58d847465cb4e126d79785
Kyle Atmore <atmorek@gmail.com>
2017-08-22 10:18:19 -0400

    Setup App and Root React components

---
+++ frontend/components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+const App = () => (
+  <div>
+    <header>
+      <h1>Kyle Fitness Pal!</h1>
+    </header>
+
+  </div>
+);
+
+export default App;
--- frontend/fitness_pal.jsx
+++ frontend/fitness_pal.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import { signup, login, logout } from './util/session_api_util';
+import Root from './components/root';
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
+
+  // For testing
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-  window.login = login;
-  window.signup = signup;
-  window.logout = logout;
-  ReactDOM.render(<h1>Kyle Fitness Pal</h1>, root);
+  // testing
+
+  ReactDOM.render(<Root store={store}/>, root);
 });
