924217abc8d08b527dab8d6a4fd21dd331309834
Kyle Atmore <atmorek@gmail.com>
2017-08-22 10:00:32 -0400

    Write Ajax request functions

--- frontend/fitness_pal.jsx
+++ frontend/fitness_pal.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { signup, login, logout } from './util/session_api_util';
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  window.login = login;
+  window.signup = signup;
+  window.logout = logout;
   ReactDOM.render(<h1>Kyle Fitness Pal</h1>, root);
 });
