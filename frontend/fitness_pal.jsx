69ea3e07973b9540c734eb7ca0452fb7d1f1893d
Kyle Atmore <atmorek@gmail.com>
2017-08-22 10:10:40 -0400

    Add session actions, root reducer, and configure store function

--- frontend/fitness_pal.jsx
+++ frontend/fitness_pal.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 import { signup, login, logout } from './util/session_api_util';
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   window.login = login;
   window.signup = signup;
   window.logout = logout;
