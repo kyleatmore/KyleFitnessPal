9b5ce0bec09dba873597275947265cc344491ab3
Kyle <atmorek@gmail.com>
2017-08-24 21:37:33 -0400

    Begin styling goal summary table

--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! user, :id, :username, :email, :height, :gender, :age
+json.extract! user, :id, :username, :email, :height, :gender, :age, :calorie_allowance
 json.extract! user.current_goal, :current_weight, :goal_weight, :activity_level, :goal_description
--- frontend/components/goals/goal_summary.jsx
+++ frontend/components/goals/goal_summary.jsx
@@ -1,69 +1,72 @@
 import React from 'react';
 
-const GoalSummary = (props) => {
+const GoalSummary = ({ currentUser }) => {
+  const { calorie_allowance } = currentUser;
   return (
-    <div className="signup-container part2">
+    <div className="signup-container part2 goals">
       <h2>Your Suggested Fitness And Nutrition Goals</h2>
-        <p>Congratulations! Your personalized diet and fitness profile is now
-        complete. Based on your answers, here are your suggested nutrition and
-        fitness goals</p>
+        <p>
+          Congratulations! Your personalized diet and
+          fitness profile is now complete. Based on your answers, here are your
+          suggested nutrition and fitness goals
+        </p>
 
       <section className="nutrition goals">
-        <table>
-          <tr>
-            <th>Nutritional Goals</th>
-            <th>Target</th>
-          </tr>
-          <tr>
-            <th>Net Calories Consumed* / Day</th>
-            <th>1,000 Calories / Day</th>
-          </tr>
-          <tr>
-            <th>Carbs / Day</th>
-            <th>100.0 g</th>
-          </tr>
-          <tr>
-            <th>Fat / Day</th>
-            <th>100.0 g</th>
-          </tr>
-          <tr>
-            <th>Protein / Day</th>
-            <th>100.0 g</th>
-          </tr>
+        <table className="collapse-borders">
+          <tbody>
+            <tr>
+              <th className="goal-header title">Nutritional Goals</th>
+              <th className="goal-header target">Target</th>
+            </tr>
+            <tr>
+              <td className="first goal-row">Net Calories Consumed* / Day</td>
+              <td className="first goal-row target">{calorie_allowance} Calories / Day</td>
+            </tr>
+            <tr>
+              <td className="goal-row">Carbs / Day</td>
+              <td className="goal-row target">{calorie_allowance * 0.5} g</td>
+            </tr>
+            <tr>
+              <td className="goal-row">Fat / Day</td>
+              <td className="goal-row target">{calorie_allowance * 0.3} g</td>
+            </tr>
+            <tr>
+              <td className="goal-row">Protein / Day</td>
+              <td className="goal-row target">{calorie_allowance * 0.2} g</td>
+            </tr>
+          </tbody>
         </table>
-        <p>
+        <p className="table-footer">
           *Net calories consumed = total calories consumed - exercise calories
           burned. So the more you exercise, the more you can eat!
         </p>
       </section>
 
       <section className="fitness goals">
-        <table>
-          <tr>
-            <th>Fitness Goals</th>
-            <th>Target</th>
-          </tr>
-          <tr>
-            <th>Calories Burned / Week / Day</th>
-            <th>1,000 Calories / Day</th>
-          </tr>
-          <tr>
-            <th>Workouts / Week</th>
-            <th>3 workouts</th>
-          </tr>
-          <tr>
-            <th>Minutes / Workout</th>
-            <th>100.0 g</th>
-          </tr>
-          <tr>
-            <th>Protein / Day</th>
-            <th>100.0 g</th>
-          </tr>
+        <table className="collapse-borders">
+          <tbody>
+            <tr>
+              <th className="goal-header title">Fitness Goals</th>
+              <th className="goal-header target">Target</th>
+            </tr>
+            <tr>
+              <td className="first goal-row">Calories Burned / Week</td>
+              <td className="first goal-row target">600 Calories / Week</td>
+            </tr>
+            <tr>
+              <td className="goal-row">Workouts / Week</td>
+              <td className="goal-row target">3 workouts</td>
+            </tr>
+            <tr>
+              <td className="goal-row">Minutes / Workout</td>
+              <td className="goal-row target">30 minutes</td>
+            </tr>
+          </tbody>
         </table>
       </section>
 
       <section className="summary">
-        <p>If you follow this plan...</p>
+        <p className="goal-footer">If you follow this plan...</p>
         <h2>Your projected weight loss is 1 lb/week</h2>
         <h2>You should lose 5lbs by September 19</h2>
       </section>
