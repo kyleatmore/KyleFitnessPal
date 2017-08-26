9fbb540aa13d660edf9ea4b9b9cad7612dc0b190
Kyle Atmore <atmorek@gmail.com>
2017-08-26 19:51:24 -0400

    Create outline of FoodDiaryIndex component

--- frontend/components/food_diaries/food_diary_index.jsx
+++ frontend/components/food_diaries/food_diary_index.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import FoodDiaryIndexItem from './food_diary_index_item';
 
 class FoodDiaryIndex extends React.Component {
   render() {
+    breakfastItems
     return (
-      <h1>Food Diary Index</h1>
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <th className="goal-header title">Breakfast</th>
+              <th className="goal-header target">Calories</th>
+              <th className="goal-header target">Carbs</th>
+              <th className="goal-header target">Fat</th>
+              <th className="goal-header target">Protein</th>
+            </tr>
+            <tr>
+              {breakfastItems}
+            </tr>
+
+            <tr>
+              <td>Lunch</td>
+            </tr>
+            <tr>
+              {lunchItems}
+            </tr>
+
+            <tr>
+              <td>Dinner</td>
+            </tr>
+            <tr>
+              {dinnerItems}
+            </tr>
+          </tbody>
+        </table>
+
+        <h1>Food Diary Total Component</h1>
+      </div>
     );
   }
 }
