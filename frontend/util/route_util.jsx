82d772e16aeb6923bec27a712b1274d826838171
Kyle Atmore <atmorek@gmail.com>
2017-09-13 10:59:07 -0400

    Modify ProtectedRoute to take additional props as input

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -13,26 +13,25 @@ const Auth = ({component: Component, path, loggedIn}) => {
           <Redirect to="/" />
         )
     )}/>
-  )
+  );
 };
 
-const Protected = ({component: Component, path, loggedIn}) => {
+const Protected = ({component: Component, path, otherProps, loggedIn}) => {
   return(
     <Route
       path={path}
-      render={(props) => (
-        loggedIn ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to="/login" />
-        )
-    )}/>
-  )
+      render={(props) => {
+        const allProps = Object.assign({}, props, otherProps);
+        return loggedIn ?
+          <Component {...allProps} /> : <Redirect to="/login" />;
+      }
+    }/>
+  );
 };
 
 const mapStateToProps = (state) => {
   return {loggedIn: Boolean(state.session.currentUser)};
-}
+};
 
-export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth))
-export const ProtectedRoute = withRouter(connect(mapStateToProps, null)(Protected))
+export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
+export const ProtectedRoute = withRouter(connect(mapStateToProps, null)(Protected));
